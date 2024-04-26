774a7cacdacf878dd4d5f47dd8ab023f71f09246
yanxinle <1020121123@qq.com>
2024-04-26 18:51:23 +0800

    研究加密解密库cryptography

--- cryptography/cryptography3.py
+++ cryptography/cryptography3.py
@@ -1,5 +1,5 @@
-from comm.common import green_input, orange_print, yellow_print
-from cryptography.fernet import Fernet
+from comm.common import green_input, orange_print, yellow_print, red_print, blue_print
+from cryptography.fernet import Fernet, InvalidToken
 
 while True:
     try:
@@ -13,20 +13,25 @@ while True:
             orange_print("已退出")
             break
         cipher_text = cipher_suite.encrypt(text.encode('utf-8')).decode('utf-8')
-        yellow_print(f"密钥: {key.decode('utf-8')}")
-        print(f"密文: {cipher_text}\n")
+        blue_print(f"密钥: {key.decode('utf-8')}")
+        yellow_print(f"密文: {cipher_text}\n")
 
         # 解密
         cipher_text = green_input("请输入要解密的内容(输入q退出): ")
         if cipher_text == 'q':
             orange_print("已退出")
             break
-        key_input = input("请输入密钥(输入q退出): ")
+        key_input = green_input("请输入密钥(输入q退出): ")
         if key_input == 'q':
             orange_print("已退出")
             break
         cipher_suite = Fernet(key_input.encode('ascii'))  # 使用用户输入的密钥创建Fernet对象
         plain_text = cipher_suite.decrypt(cipher_text.encode('utf-8')).decode('utf-8')
-        print(f"明文: {plain_text}\n")
-    except ValueError as v:
-        print(f"错误: {v}\n")
+        yellow_print(f"明文: {plain_text}\n")
+    except ValueError:
+        red_print("错误: 无效的密钥, 请输入32个URL安全的base64编码字节\n")
+    except InvalidToken as i:
+        red_print("错误: 解密失败, 密钥或密文无效\n")
+    except KeyboardInterrupt:
+        red_print("\n程序已强制中断")
+        break
