0ddee168dbf10875dcd0c7061d81c0dcad887f47
yanxinle <1020121123@qq.com>
2024-04-27 19:52:58 +0800

    研究加密解密库cryptography

--- cryptography/cryptography4.py
+++ cryptography/cryptography4.py
@@ -20,36 +20,37 @@ def decryption():
 
 
 window = tk.Tk()
-EasyAutoWindow(window, window_title="CryptographyMethod", minimum_value_x=800, minimum_value_y=500,
-               maximum_value_x=1640, maximum_value_y=980, window_height_value=720, window_width_value=1150)
+EasyAutoWindow(window, window_title="CryptographyMethod", minimum_value_x=1312, minimum_value_y=876,
+               maximum_value_x=1640, maximum_value_y=980, window_width_value=1400, window_height_value=890)
 
 f1 = EasyFrame(window, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
 f11 = EasyFrame(f1, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f111 = EasyFrame(f11, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f112 = EasyFrame(f11, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f113 = EasyFrame(f11, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-
 f12 = EasyFrame(f1, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f121 = EasyFrame(f12, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f122 = EasyFrame(f12, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
-f123 = EasyFrame(f12, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f13 = EasyFrame(f1, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f14 = EasyFrame(f1, fill=tk.BOTH, is_debug=True, side=tk.RIGHT, expand=tk.YES).get()
 
-f2 = EasyFrame(window, fill=tk.BOTH, is_debug=True, side=tk.TOP).get()
+f2 = EasyFrame(window, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f21 = EasyFrame(f2, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f22 = EasyFrame(f2, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f23 = EasyFrame(f2, fill=tk.BOTH, is_debug=True, side=tk.TOP, expand=tk.YES).get()
+f24 = EasyFrame(f2, fill=tk.BOTH, is_debug=True, side=tk.RIGHT, expand=tk.YES).get()
 
-EasyLabel(f111, text="加密:", side=tk.LEFT)
-EasyButton(f113, text="加密", side=tk.TOP, fill=tk.BOTH)
-EasyMultiText(f111, fill=tk.BOTH, side=tk.TOP, expand=tk.YES)
+EasyLabel(f11, text="要加密的文本:", side=tk.LEFT)
+EasyMultiText(f11, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyLabel(f12, text="加密时的密钥:", side=tk.LEFT)
+EasyMultiText(f12, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyLabel(f13, text="加密后的文本:", side=tk.LEFT)
+EasyMultiText(f13, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyButton(f14, text="加密", fill=tk.BOTH, side=tk.TOP, expand=tk.YES, height=2)
 
-EasyLabel(f121, text="密钥:", side=tk.LEFT)
-EasyMultiText(f121, fill=tk.BOTH, side=tk.TOP, expand=tk.YES)
+EasyLabel(f21, text="要解密的文本:", side=tk.LEFT)
+EasyMultiText(f21, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyLabel(f22, text="解密时的密钥:", side=tk.LEFT)
+EasyMultiText(f22, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyLabel(f23, text="解密后的文本:", side=tk.LEFT)
+EasyMultiText(f23, fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+EasyButton(f24, text="解密", fill=tk.BOTH, side=tk.TOP, expand=tk.YES, height=2)
 
-EasyLabel(f112, text="密钥:", side=tk.LEFT)
-EasyMultiText(f112, fill=tk.BOTH, side=tk.TOP, expand=tk.YES)
-
-EasyLabel(f122, text="解密:", side=tk.LEFT)
-EasyButton(f122, text="解密", side=tk.RIGHT, fill=tk.BOTH)
-EasyMultiText(f122, fill=tk.BOTH, side=tk.TOP, expand=tk.YES)
-
-EasyButton(f2, text="退出", expand=tk.YES, fill=tk.BOTH, height=2, cmd=quit_window)
+EasyButton(window, text="退出", fill=tk.BOTH, side=tk.TOP, expand=tk.NO, height=2)
 
 window.mainloop()
