d5eaec943b66af873dc72053ba8106b40625368b
yanxinle <1020121123@qq.com>
2024-05-31 18:58:15 +0800

    研究AEAD加密方法

---
+++ AEAD/AEAD2.py
@@ -0,0 +1,32 @@
+import os
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+
+backend = default_backend()
+
+
+def encrypt(plaintext, key, nonce):
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=backend)
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(plaintext)
+    return ciphertext
+
+
+def decrypt(ciphertext, key, nonce):
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=backend)
+    decryptor = cipher.decryptor()
+    decrypted_text = decryptor.update(ciphertext)
+    return decrypted_text
+
+
+key = os.urandom(32)
+nonce = os.urandom(16)
+plaintext = b'This is a test.'
+
+ciphertext = encrypt(plaintext, key, nonce)
+print("密文:", ciphertext.hex())
+print("密钥:", key.hex())
+
+decrypted_text = decrypt(ciphertext, key, nonce)
+print("明文:", decrypted_text.hex())
