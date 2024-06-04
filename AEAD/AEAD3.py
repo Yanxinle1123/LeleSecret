6e363b0b16d5c1498b308719464b0421ec700dee
yanxinle <1020121123@qq.com>
2024-06-04 13:07:24 +0800

    研究AEAD加密方法

--- AEAD/AEAD3.py
+++ AEAD/AEAD3.py
@@ -1,5 +1,6 @@
 import os
 
+from comm.common import green_input, orange_print, blue_print, yellow_print, red_print
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
@@ -24,24 +25,24 @@ while True:
     try:
         key = os.urandom(32)
         nonce = os.urandom(16)
-        plaintext = input("请输入需要加密的文字(输入q退出): ")
+        plaintext = green_input("请输入需要加密的文字(输入q退出): ")
         if plaintext == 'q':
-            print("已退出")
+            orange_print("已退出")
             break
         ciphertext = encrypt(plaintext.encode('utf-8'), key, nonce)
-        print(f"密文: {ciphertext.hex()}")
-        print(f"密钥: {key.hex()}")
-        ciphertext_input = input("\n请输入需要解密的文字(输入q退出): ")
+        yellow_print(f"密文: {ciphertext.hex()}")
+        blue_print(f"密钥: {key.hex()}")
+        ciphertext_input = green_input("\n请输入需要解密的文字(输入q退出): ")
         if ciphertext_input == 'q':
-            print("已退出")
+            orange_print("已退出")
             break
-        key_input = input("请输入密钥(输入q退出): ")
+        key_input = green_input("请输入密钥(输入q退出): ")
         if key_input == 'q':
-            print("已退出")
+            orange_print("已退出")
             break
         ciphertext_bytes = bytes.fromhex(ciphertext_input)
         key_bytes = bytes.fromhex(key_input)
         decrypted_text = decrypt(ciphertext_bytes, key_bytes, nonce)
-        print(f"明文: {decrypted_text.decode('utf-8')}\n")
+        yellow_print(f"明文: {decrypted_text.decode('utf-8')}\n")
     except ValueError:
-        print("解密失败, 无效的密文或密钥\n")
+        red_print("解密失败, 无效的密文或密钥\n")
