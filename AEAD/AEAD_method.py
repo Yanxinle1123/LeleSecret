bca589bbfe0ae1125f8860564d25b03d59b2a838
yanxinle <1020121123@qq.com>
2024-06-05 13:34:05 +0800

    研究AEAD加密方法

--- AEAD/AEAD_method.py
+++ AEAD/AEAD_method.py
@@ -1,7 +1,6 @@
 import os
 
 from comm.common import green_input, orange_print, blue_print, yellow_print, red_print
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
 
@@ -10,29 +9,27 @@ class AEADEncryptionMethod:
         self._text = text.encode('utf-8')
 
     def encryption(self):
-        backend = default_backend()
         key = os.urandom(32)
         nonce = os.urandom(16)
-        cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=backend)
+        cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None)
         encryptor = cipher.encryptor()
         ciphertext = encryptor.update(self._text)
-        key = f"{key.hex()}+{nonce.hex()}+{str(backend)}"
+        key = f"{key.hex()}+{nonce.hex()}"
         return ciphertext.hex(), key
 
 
 class AEADDecryptionMethod:
     def __init__(self, text, key):
-        self._text = bytes.fromhex(text)  # Convert ciphertext_input to bytes
+        self._text = bytes.fromhex(text)
         self._key = key
 
     def decryption(self):
         decompose_text = self._key.split('+')
         key = decompose_text[0]
         nonce = decompose_text[1]
-        backend = decompose_text[2]
         key_bytes = bytes.fromhex(key)
         nonce_bytes = bytes.fromhex(nonce)
-        cipher = Cipher(algorithms.ChaCha20(key_bytes, nonce_bytes), mode=None, backend=backend)
+        cipher = Cipher(algorithms.ChaCha20(key_bytes, nonce_bytes), mode=None)
         decryptor = cipher.decryptor()
         decrypted_text = decryptor.update(self._text)
         return decrypted_text.decode('utf-8')
@@ -60,3 +57,5 @@ if __name__ == '__main__':
             blue_print(f"明文: {decrypted_text}\n")
         except ValueError:
             red_print("解密失败, 无效的密文或密钥\n")
+        except IndexError:
+            red_print("解密失败, 无效的密钥\n")
