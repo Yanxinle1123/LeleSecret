176448b8bbbe763280379f175b89b0d9f4fc813f
yanxinle <1020121123@qq.com>
2024-06-19 13:07:18 +0800

    研究Camellia算法

--- Camellia/Camellia1.py
+++ Camellia/Camellia1.py
@@ -1,28 +1,33 @@
 import os
 
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 def camellia_encrypt(plaintext, key):
+    padder = padding.PKCS7(128).padder()  # 128 is block size for Camellia
+    padded_data = padder.update(plaintext) + padder.finalize()
     cipher = Cipher(algorithms.Camellia(key), modes.ECB(), backend=default_backend())
     encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(plaintext)
+    ciphertext = encryptor.update(padded_data)
     return ciphertext
 
 
 def camellia_decrypt(ciphertext, key):
     cipher = Cipher(algorithms.Camellia(key), modes.ECB(), backend=default_backend())
     decryptor = cipher.decryptor()
-    plaintext = decryptor.update(ciphertext)
+    padded_data = decryptor.update(ciphertext)
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(padded_data) + unpadder.finalize()
     return plaintext
 
 
 # 生成一个16字节的随机密钥
 key = os.urandom(16)
 
-# 输入需要加密的文本，确保其长度为16的倍数
-plaintext = b"Hello, World!123"
+# 输入需要加密的文本
+plaintext = b"Hello, World! This is a test."
 
 # 使用Camellia加密
 ciphertext = camellia_encrypt(plaintext, key)
