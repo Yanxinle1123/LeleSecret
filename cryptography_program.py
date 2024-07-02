b2cda5b9542a6bd7daf56291ac31574d01b7b054
yanxinle <1020121123@qq.com>
2024-07-02 17:08:20 +0800

    更新 修复了居中窗口不准的问题

--- cryptography_program.py
+++ cryptography_program.py
@@ -620,10 +620,13 @@ def center_window(root):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    x = (screen_width - width) / 2
-    y = (screen_height - height) / 2
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2 - 20
 
     root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    root.lift()
+    root.focus_set()
 
 
 def settings():
@@ -686,6 +689,9 @@ def settings():
         f11 = EasyFrame(f1, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
         f12 = EasyFrame(f1, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
         f13 = EasyFrame(f1, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
+        f14 = EasyFrame(f1, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
+        f141 = EasyFrame(f14, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
+        f142 = EasyFrame(f14, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
         f2 = EasyFrame(settings_window, fill=tk.BOTH, side=tk.TOP, expand=tk.YES, is_debug=False).get()
 
         EasyLabel(f11, text="加密算法:", side=tk.LEFT)
@@ -699,6 +705,9 @@ def settings():
         other_settings = EasyCheckButton(f13, text=["退出设置未保存时提醒", "加密解密出错时弹出错误提示",
                                                     "重置设置后自动保存", "启用快捷键"],
                                          set_text_list=other_settings_set, master_win=window, expand=True, fill=tk.Y)
+
+        EasyLabel(f141, text="1. 按下q键退出", side=tk.LEFT, font_size=12)
+        EasyLabel(f142, text="2. 按下F1打开使用说明", side=tk.LEFT, font_size=12)
 
         EasyButton(f2, text="保存", expand=tk.YES, height=2, cmd=save_settings, side=tk.LEFT,
                    fill=tk.X)
@@ -722,7 +731,6 @@ def settings():
 
     else:
         center_window(settings_window)
-        settings_window.lift()
 
 
 def instructions():
@@ -735,11 +743,11 @@ def instructions():
                              "框内。您可以在设置窗口里面调整加密的算法, 默认为自动\n\n\n解密方法: 将密文和密钥输入到指定的文本框内, 然"
                              "后点击解密按钮, 解密后的文本就会显示在指定的文本框内, 程序会根据密钥自动匹配解密算法。(注: 如果解密出错,"
                              " 程序会弹出错误提示, 如果没有看见弹窗, 可能是被设置或者其他窗口挡住了)\n\n\n设置说明: 在设置中, 你可以"
-                             "设置加密和解密的算法和其他的功能。如果您想要恢复默认设置, 请点击重置按钮。如果您想要保存您的更改, 请点击"
-                             "保存按钮。如果您想要退出设置, 请点击退出按钮。\n\n\n关于设置: 点击加密解密窗口下方的设置按钮, 程序就会弹"
-                             "出设置窗口。在设置里, 您可以选择加密解密的算法。\n\n\n注意事项: 请不要全屏显示窗口, 全屏模式下, 显示会有"
-                             "一些问题。\n\n\n快捷键: 您可以通过按q键来关闭程序, 您也可以通过按command键加逗号来打开设置窗口, 您还可"
-                             "以按F1键来打开使用方法窗口。")
+                             "设置加密的算法和其他的功能。(注: 由于程序会根据密钥自动检测加密的算法来匹配解密的算法, 所以无需设置解密的"
+                             "算法)如果您想要恢复默认设置, 请点击重置按钮。如果您想要保存您的更改, 请点击保存按钮。如果您想要退出设置, "
+                             "请点击退出按钮。\n\n\n关于设置: 点击加密解密窗口下方的设置按钮, 程序就会弹出设置窗口。在设置里, 您可以选"
+                             "择加密解密的算法。\n\n\n注意事项: 请不要全屏显示窗口, 全屏模式下, 显示会有一些问题。\n\n\n快捷键: 您可"
+                             "以通过按q键来关闭程序, 您也可以通过按command键加逗号来打开设置窗口, 您还可以按F1键来打开使用方法窗口。")
 
         EasyAutoWindow(instructions_window, window_title="使用方法", window_width_value=600, window_height_value=400,
                        minimum_value_x=230, minimum_value_y=170)
@@ -760,7 +768,6 @@ def instructions():
             instructions_window.bind('<q>', lambda event: quit_window())
     else:
         center_window(instructions_window)
-        instructions_window.lift()
 
 
 settings_window = None
