b48d4c49607df0869f61c8f818db95d168ba3db4
yanxinle <1020121123@qq.com>
2024-07-24 18:43:59 +0800

    更新 -修复了一些已知问题

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ root.title("示例")
 fruit_check_button = EasyCheckButton(root,
                                      text=["苹果(默认)", "香蕉(默认)", "橙子(默认)", "葡萄", "梨子(默认)", "榴莲",
                                            "荔枝", "草莓", "柚子", "樱桃", "杏子", "菠萝", "西瓜"],
-                                     set_=["苹果(默认)", "梨子(默认)", "橙子(默认)", "香蕉(默认)"],
+                                     set_text_list=["苹果(默认)", "梨子(默认)", "橙子(默认)", "香蕉(默认)"],
                                      expand=tkinter.YES)
 
 button = tkinter.Button(root, text="改变选中值", command=on_button_click)
